53c57045c29bae1b0cdaeeb97a7a32db1b8bf88e
jaisondavid-m <jaison7373@gmail.com>
2025-10-16 09:19:59 +0530

    Img

--- src/Component/NavBar/NavBar.jsx
+++ src/Component/NavBar/NavBar.jsx
@@ -3,7 +3,7 @@ import './NavBar.css'
 function NavBar() {
   return (
     <div className='header'>
-      <h1><img src='./public/Logo.png'/>BIT CENTERAL</h1>
+      <h1><img src='../../../public/Logo.png'/>BIT CENTERAL</h1>
     </div>
   )
 }
