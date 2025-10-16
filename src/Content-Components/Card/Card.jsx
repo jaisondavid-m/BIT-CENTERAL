ce6834c3bafe9d77fbcda6292885cf6906e5af25
jaisondavid-m <jaison7373@gmail.com>
2025-10-16 11:24:33 +0530

    style to card

--- src/Content-Components/Card/Card.jsx
+++ src/Content-Components/Card/Card.jsx
@@ -9,7 +9,8 @@ function Card({name,link,img,btntext}) {
   return (
     <div className='card'>
       <div className="img">
-        <img src={img} alt="" style={{display:"block",height:"300px", width:"300px",objectFit:"cover"}}/>
+        <img src={img} alt="" style={{display:"block",height:"300px", width:"max-content",borderRadius:"10px"}}/>
+        <hr style={{marginTop:"20px"}}/>
       </div>
       <div className="cardbottom">
         <h3>{name}</h3>
