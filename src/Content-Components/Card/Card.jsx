4b1810c480a6dc57050d12419ad054d437293d77
jaisondavid-m <jaison7373@gmail.com>
2025-10-16 11:00:43 +0530

    Card img fix

--- src/Content-Components/Card/Card.jsx
+++ src/Content-Components/Card/Card.jsx
@@ -9,7 +9,7 @@ function Card({name,link,img,btntext}) {
   return (
     <div className='card'>
       <div className="img">
-        <img src={img} alt="" style={{height:"300px", width:"300px"}}/>
+        <img src={img} alt="" style={{height:"300px", width:"max-content"}}/>
       </div>
       <div className="cardbottom">
         <h3>{name}</h3>
@@ -18,5 +18,4 @@ function Card({name,link,img,btntext}) {
     </div>
   )
 }
-
 export default Card
