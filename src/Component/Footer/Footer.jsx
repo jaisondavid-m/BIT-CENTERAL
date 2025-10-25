7e665b08818a37d2efb7bf59b151a346ea57ff15
jaisondavid-m <jaison7373@gmail.com>
2025-10-25 17:19:27 +0530

    edited footer

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -4,8 +4,8 @@ import './Footer.css'
 function Footer() {
   return (
     <div className='footer ' style={{display:"fixed",bottom:"0px"}}>
-       <h1>&copy; The SSG Boys</h1>
-       <p>All rights Reserved</p>
+       <p>&copy; All rights Reserved</p>
+       <a href="https://www.linkedin.com/in/jaison-david-m-a14072360/" target="_blank"><p className='font-bold'>Crafted by an average 1st-year boy</p></a>
     </div>
   )
 }
