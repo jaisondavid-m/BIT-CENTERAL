48f5927e02268a2f94b5afc41ca15d1653f4b5c6
jaisondavid-m <jaison7373@gmail.com>
2025-11-02 12:15:23 +0530

    Added sreachBar

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,19 +4,33 @@ import data from "../../../Data/Data.json"
 import { useAuthState } from "react-firebase-hooks/auth"
 import { auth } from './../../Firebase';
 
-import React from 'react'
+import React, { useState } from 'react'
 
 function Home({ name, link, img }) {
 
   const [user] = useAuthState(auth);
+  const [sreach,Setsreach]=useState("");
+
+  const handleSreach=(e)=>{
+    Setsreach(e.target.value);
+  }
+
+  const FilteredData = data.cards.filter((cards)=>
+    cards.name.trim().toLowerCase().includes(sreach.toLowerCase())
+  )
 
   return (
     <div>
-      <div className='border-b-2 w-max-5 text-center font-bold text-blue-900 text-4xl mx-auto m-5 p-3'>Welcome {user ? user.displayName : "Guest"}</div>
+      <div className='border-b-2  text-center font-bold md:text-blue-900 md:text-4xl text-blue-500 text-2xl p-2'>Welcome {user ? user.displayName : "Guest"}</div>
+      <input type="text"
+       placeholder='Sreach...'
+       onChange={handleSreach}
+       className="block cosmic-input w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl mx-auto my-6 px-5 py-3.5 rounded-2xl bg-gradient-to-r from-slate-50 to-slate-100 border-2 border-slate-300 hover:border-slate-400 focus:outline-none focus:border-transparent focus:ring-4 focus:ring-blue-500/50 shadow-lg hover:shadow-xl focus:shadow-2xl transition-all duration-300 ease-in-out placeholder:text-slate-400 text-slate-800 font-medium"
+      />
       <div className="cardContainer">
 
         {
-          data.cards.map((card, index) => (
+          FilteredData.map((card, index) => (
             <Card
               key={index}
               name={card.name}
