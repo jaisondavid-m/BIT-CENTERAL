ac0a8187c27ff3ea2960842ae3bc925219aede81
jaisondavid-m <jaison7373@gmail.com>
2025-12-17 22:10:08 +0530

    added mat answerkey

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import NavBar from './Component/NavBar/NavBar'
 import './App.css'
 import Footer from './Component/Footer/Footer'
 import Home from './Pages/Home/Home'
-import Contact from './Pages/Contact/Contact'
 import About from './Pages/About/About'
 import { Route,Routes,Navigate, useLocation } from 'react-router-dom'
 
@@ -16,14 +15,15 @@ function App() {
 
       
       <NavBar/>
+
       <Routes>
         
         <Route path='/' element={<Navigate to="/home" />}/>
         <Route path='/home' element={<Home/>}/>
-        <Route path='/contact' element={<Contact/>}/>
         <Route path='/about' element={<About/>}/>
         
       </Routes>
+      
       <Footer/>
     </div>
   )
