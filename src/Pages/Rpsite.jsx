9497f1dc04facb377b14c9d57a6e295dc27e2127
Jaison David M <jaison7373@gmail.com>
2026-02-16 19:11:56 +0530

    fixed mobile view

--- src/Pages/Rpsite.jsx
+++ src/Pages/Rpsite.jsx
@@ -8,7 +8,7 @@ function Rpsite() {
   return (
     <div>
       {loading && <FullScreenLoader/>}
-      <iframe className='w-screen h-screen block border-none' id="rewardFrame" src="https://script.google.com/macros/s/AKfycbxQARjvR6fKHRV0ry_g7NPLe4LK22snH8zkVyG44CN_7g_YQR3yyYgEn60-wrYjVgSuBA/exec" onLoad={() => setTimeout(() => setLoading(false), 500)} title="RP Site"></iframe>
+      <iframe className='w-screen h-screen block border-none' id="rewardFrame" src="https://script.google.com/macros/s/AKfycbzyDfs25UGZnrRfXQbtcXkCh6C0WrXc2KR0zbh3smbA6Q7JEE7_0hrL9S_Go5npZpMXGQ/exec" onLoad={() => setTimeout(() => setLoading(false), 500)} title="RP Site"></iframe>
       <button onClick={() => navigate("/")} className="fixed bottom-6 right-6 z-50 flex items-center gap-2 px-4 py-2 rounded-full bg-blue-600 text-white text-sm font-semibold shadow-lg hover:bg-blue-700 hover:scale-105 transition-all">⬅ Home</button>
     </div>
   )
